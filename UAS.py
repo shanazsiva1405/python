0e0505a44e958d1df25ce50e0adfb487d45287dc
shanazsiva1405 <shanazsivanasra8@gmail.com>
2024-01-16 10:52:44 +0700

    first commit

---
+++ UAS.py
@@ -0,0 +1,32 @@
+def anggota():
+    print("\n===================================================================================")
+    print("\t   PROJECT AKHIR KONSEP PEMROGRAMAN  ")
+    print("\t   Aplikasi Pemesanan Tiket Bioskop     ")
+    print("=====================================================================================\n")
+    print("\n------------------------------------------------------------------------------------")
+    print("\t   ||     Kelompok Kubu Dalam     ||   ")
+    print("--------------------------------------------------------------------------------------")
+    print("\t 1. Mohammad Haekal                        (2307421045) ")
+    print("\t 2. Rafi Forza Prasetyo                    (2307421036) ")
+    print("\t 3. Hansha Natalegawa                      (2307421054) ")
+    print("\t 4. Farrel Arya Putra                      (2307421045) ")
+    print("\t 5. Valenzky Tesalonika Pangaribuan        (2307421045) ")
+    print("\t 6. Shanazsiva Nasra                       (2307421045) ")
+    print("\t 7. Muhammad Irfadillah                    (2307421048) ")
+    print(input("tekan [enter) untuk melanjutkan Aplikasi"))
+
+def namafilm():
+    print ("\n")
+    print ("====================================================================================")
+    print ("\tSELAMAT DATANG PARA PENONTON")
+    print ("\t\t KUBU DALAM XXI")
+    print ("=====================================================================================\n)
+    print ("-------------------------------------------------------------------------------------")
+    print ("|| Kode\t\t                  Judul\t\t                  Harga  ||")
+    print (
+    print ("|| BDJ\t\t              13 Bom Di Jakarta\t\t          50.0000 ||")
+    print ("|| ACK\t\t                 Anchika 1995\t\t            50.0000 ||")
+    print ("|| Aqm\t\t                    Aquaman\t\t              40.0000 ||")
+    print ("|| SS\t\t                 Sehidup Semati\t\t           40.0000 ||")
+    print ("|| SN\t\t                  Siksa Neraka\t\t            40.0000 ||")
+   
